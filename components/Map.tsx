56e6a358189aaafa1512b436491482875eb4038f
Paul Hernandez <fdefer.dev@gmail.com>
2024-11-18 12:10:41 +0100

    Add province data integration

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,27 +1,38 @@
 "use client";
 
-
 import "leaflet/dist/leaflet.css";
-
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
-
 import "leaflet-defaulticon-compatibility";
-
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { useEffect, useState } from "react";
 
 export default function Map() {
+  const [provincesData, setProvincesData] = useState(null);
+
+  useEffect(() => {
+    const fetchProvincesData = async () => {
+      const response = await fetch("/data/spain-provinces.geojson");
+      const data = await response.json();
+      setProvincesData(data);
+    };
+
+    fetchProvincesData();
+  }, []);
+
   return (
     <MapContainer
       preferCanvas={true}
       center={[40, -3.7492]}
       zoom={7}
       scrollWheelZoom={true}
-      style={{ height: "100vh", width: "100wh" }}
+      style={{ height: "100vh", width: "100vw" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {provincesData && <GeoJSON data={provincesData} />}
     </MapContainer>
   );
 }
